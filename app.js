f919eec178fb10592e2f28d8b7b48170b0af39a7
임재혁 <wogur091511@naver.com>
2023-02-22 02:10:26 +0000

    리스트 형태를 통해서 항목추가하게 설정

--- app.js
+++ app.js
@@ -3,22 +3,47 @@ const menu = [
     id: 1,
     title: "돈코츠라멘",
     category: "면류",
-    price: 15.99,
-    img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    price: 9000,
+    img: "https://ldb-phinf.pstatic.net/20221014_62/1665708036055SUqhu_JPEG/bKL-PvLxkNiE2REsYOCN_av0tpKDKeNqHfffLe12nqw%3D.jpg",
+    desc: `쫄깃한 생면과 부드러운 차슈, 반숙 계란으로 실패가 없는 메뉴입니다. `,
   },
   {
     id: 2,
-    title: "diner double",
-    category: "lunch",
-    price: 13.99,
-    img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    title: "매운 돈코츠라멘",
+    category: "면류",
+    price: 9900,
+    img: "https://ldb-phinf.pstatic.net/20221014_69/1665708036157AzCVl_JPEG/SNcEYrGTl8XiEmWexipywJ15QBEy6Yy6pd8Dg_AjgZY%3D.jpg",
+    desc: `돈코츠라멘의 매운버전으로 매콤한 취향이신 분들에게 강추드리는 메뉴 입니다.`,
   },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener("DOMContentLoaded", function() {
-	console.log("돈코츠라멘");
+	displayMenuItems(menu);
 });
+
+function displayMenuItems(menuItems) {
+	let displayMenu = menuItems.map(function(item) {
+		// console.log(item);
+		
+		return `<article class="menu-item">
+				<img src=${item.img} class="photo" alt=${item.title}/>
+				<div class="item-info">
+					<header>
+						<h4>
+							${item.title}
+						</h4>
+						<h4 class="price">
+							₩${item.price}
+						</h4>
+					</header>
+					<p class="item-text">
+						${item.desc}
+					</p>
+				</div>
+			</article>`;
+    });
+	displayMenu = displayMenu.join("");
+	sectionCenter.innerHTML = displayMenu;
+}
