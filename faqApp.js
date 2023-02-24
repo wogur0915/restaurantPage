0a26f557651bdefb8a15979a18e45ebe6842b657
임재혁 <wogur091511@naver.com>
2023-02-24 02:03:22 +0000

    모바일 해상도에 맞게 지도 조정 완료

--- faqApp.js
+++ faqApp.js
@@ -26,3 +26,28 @@ questions.forEach(function (question) {
 previous.addEventListener("click", function() {
 	window.history.back();
 });
+
+var mapOptions = {
+   center: new naver.maps.LatLng(36.6322344, 127.4582594),
+   zoom: 19
+};
+
+var map = new naver.maps.Map('map', mapOptions);
+
+var markerOptions = {
+    position: new naver.maps.LatLng(36.6322344, 127.4582594),
+    map: map,
+    title: '면식당 충북대점'
+};
+var marker = new naver.maps.Marker(markerOptions);
+
+var center = map.getCenter();
+marker.setPosition(center);
+
+var infoWindowOptions = {
+    content: '<div style="padding:5px;">면식당 충북대점</div>'
+};
+var infoWindow = new naver.maps.InfoWindow(infoWindowOptions);
+naver.maps.Event.addListener(marker, 'click', function() {
+    infoWindow.open(map, marker);
+});
